0828ebd53300301834556524176ba0272a0e2881
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-09-21 15:13:37 +0500

    Add files via upload

---
+++ 2.1 dars/main.py
@@ -0,0 +1,45 @@
+
+# for i in range(1, 10):
+#     print('#' * i)
+    
+# result = '#'
+
+# for i in range(1, 100):
+#     print(result * i)
+#     if i == 50:
+#      break
+ 
+# user = ['Mirsaid', 'Olmazor tumani', '974777220']
+
+# print(f"Ismi : {user[0]}, Tumani : {user[1]}, Raqami : {user[2]}")
+
+
+# juft_sonlar = []
+
+# for i in range (1, 101):
+#  if i % 2 == 0: 
+#     juft_sonlar.append(i)
+     
+# print(juft_sonlar)
+
+sonlar = [2, 6, 5, 8, 3, 5, 4] 
+
+
+katta = max(sonlar)
+
+print(katta)
+
+kichik = min(sonlar)
+
+print(kichik)
+
+sonlar.reverse()
+
+
+print(sonlar)
+
+sonlar.sort()
+
+print(sonlar)
+
+print(len(sonlar))
